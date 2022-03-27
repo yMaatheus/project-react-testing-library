61d7eaad170bd90bb25ab5f1f8ac92bbbefd5855
Matheus <stvgamer74@gmail.com>
2022-03-27 05:16:23 -0300

    R04: Testando se a pagina contem titulo "Page request not found" e se contem a imagem correta do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando componente NotFound', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aleatório');
+  });
+
+  test('A pagina contém o titulo "Page requested not found"', async () => {
+    const attr = { name: /page requested not found/i, level: 2 };
+    const pageRequestNotFound = await screen.findByRole('heading', attr);
+    expect(pageRequestNotFound).toBeInTheDocument();
+  });
+
+  test('A pagina contém a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', async () => {
+    const alt = /Pikachu crying because the page requested was not found/i;
+    const img = await screen.findByAltText(alt);
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
